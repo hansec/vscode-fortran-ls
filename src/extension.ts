909dba00fcf9160d13ce693f96a3c719556d58a2
Chris Hansen <hansec@uw.edu>
2018-06-28 13:32:14 -0400

    Add automatic version check when package loads

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,52 @@
 'use strict';
 
 import { spawn }  from 'child_process';
+const https = require('https');
 import { commands, window, workspace, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+async function checkVersion(executablePath: string) {
+	function compareSymantic(ver: string[], rec_ver: string[]) {
+		for (var index = 0; index < rec_ver.length; ++index) {
+		  if (parseInt(ver[index]) < parseInt(rec_ver[index])) {
+			return true;
+		  } else if (parseInt(ver[index]) > parseInt(rec_ver[index])) {
+			return false;
+		  }
+		}
+		return false;
+	}
+	async function checkVersionLocal(executablePath: string, rec_ver_str: string = "0.6.1") {
+		const childProcess = spawn(executablePath, ["--version"]);
+		// Check version
+		childProcess.stdout.on('data', (data) => {
+			let ver_str = data.toString().trim();
+			if (compareSymantic(ver_str.split("."), rec_ver_str.split("."))) {
+				window.showWarningMessage(`fortran-ls: A newer version (${rec_ver_str}) of "fortls" is available.`);
+			}
+		});
+		childProcess.on("error", () => {
+			const selected = window.showErrorMessage("Error spawning fortls: This can occur if you do not have the fortran-language-server installed or if it is not in your path.", 'Open settings');
+			selected.then( () =>
+				commands.executeCommand('workbench.action.openGlobalSettings')
+			)
+		});
+	}
+	https.get('https://pypi.org/pypi/fortran-language-server/json', (resp: any) => {
+		let data = '';
+		// A chunk of data has been recieved
+		resp.on('data', (chunk: string) => { data += chunk; } );
+		// The whole response has been received, check version
+		resp.on('end', () => {
+			try {
+				let package_data = JSON.parse(data);
+				checkVersionLocal(executablePath, package_data.info.version);
+			} catch (e) {
+				checkVersionLocal(executablePath);
+			}
+		});
+	}).on("error", () => { checkVersionLocal(executablePath); } );
+}
 
 export async function activate(context: ExtensionContext) {
 
@@ -14,34 +58,9 @@ export async function activate(context: ExtensionContext) {
 	const conf = workspace.getConfiguration('fortran-ls', null);
 	const executablePath = conf.get<string>('executablePath') || 'fortls';
 
-	// Check path (if fortls is available and version is ^0.3.6)
-	let args_check = ["--version"];
-	const childProcess = spawn(executablePath, args_check);
-	// Check version
-	if (conf.get<boolean>('displayVerWarning')) {
-		childProcess.stdout.on('data', (data) => {
-			let ver_str = data.toString().trim();
-			let ver_split = ver_str.split(".");
-			const rec_ver_str = "0.6.0";
-			let rec_ver = rec_ver_str.split(".");
-			for (var index = 0; index < rec_ver.length; ++index) {
-				if (parseInt(ver_split[index]) < parseInt(rec_ver[index])) {
-					window.showWarningMessage(`fortran-ls: Installed version (${ver_str}) of "fortls" is lower than the recommended version (${rec_ver_str}).`);
-					break
-				} else if (parseInt(ver_split[index]) > parseInt(rec_ver[index])) {
-					break
-				}
-			}
-		});
-	}
-	childProcess.on("error", () => {
-		const selected = window.showErrorMessage("Error spawning fortls: This can occur if you do not have the fortran-language-server installed or if it is not in your path.", 'Open settings');
-		selected.then( () =>
-			commands.executeCommand('workbench.action.openGlobalSettings')
-		)
-	});
+	if (conf.get<boolean>('displayVerWarning')) { checkVersion(executablePath) }
 
-	//
+	// Setup server arguments
 	let args_server = [];
 	if (!conf.get<boolean>('includeSymbolMem')) { args_server.push("--symbol_skip_mem") }
 	if (conf.get<boolean>('incrementalSync')) { args_server.push("--incrmental_sync") }
